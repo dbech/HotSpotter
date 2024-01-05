41c2c088fc9ac5b5d49fedc597a9430178984fce
Daniel Bech <daniel@bech.au>
2024-01-05 21:30:55 +0800

    all data assembled, file tidy. math time.

--- hotspotter.py
+++ hotspotter.py
@@ -1,45 +1,45 @@
-import numpy as np
+import numpy
 import xmltodict
 from thermal import Thermal
 from PIL import Image
 
-TEMPERATURE_MASK = 145 #we're looking for tempuratures in cencius greater than this value.
+TEMPERATURE_MASK = 145 # we're looking for tempuratures in cencius greater than this value.
+CAMERA_FOCAL_LENGTH = 38 # millimeter full frame equiv https://enterprise.dji.com/mavic-2-enterprise-advanced/specs 
+CAMERA_SENSOR_HEIGHT = 24 # millimeter full frame equiv
+CAMERA_SENSOR_WIDTH = 36 # millimeter full frame equiv
 
-flight_h = 113 #meters (need to source from image, got this from metadata)
-camera_focal_length = 38 #millimeter full frame equiv https://enterprise.dji.com/mavic-2-enterprise-advanced/specs 
-camera_sensor_h = 24 #millimeter full frame
-camera_sensor_w = 36 #millimeter full frame
+def calculate_gsd(camera_altitude, camera_sensor_mm, camera_focal_length, image_px):
+    return ((camera_altitude*1000) * camera_sensor_mm) / (camera_focal_length * image_px)
 
-image_path = 'input/DJI_0127_T.JPG'
+image_path = 'input/DJI_0007_T.JPG' # @todo: change to recusively work through a folder
 image = Image.open(image_path)
-image_h = image.height
-image_w = image.width
+image_height = image.height
+image_width = image.width
 image.close()
 
 file = open(image_path, encoding = 'latin-1')
 file_bytes = file.read()
 file.close()
-xmp_str = file_bytes[file_bytes.find('<x:xmpmeta'):file_bytes.find('</x:xmpmeta')+12]
-xmp_dict = xmltodict.parse(xmp_str)
+xmp_str = file_bytes[file_bytes.find('<x:xmpmeta'):file_bytes.find('</x:xmpmeta')+12] # look for everything enclosed in the root of our XMP data
+xmp_dict = xmltodict.parse(xmp_str).get('x:xmpmeta').get('rdf:RDF').get('rdf:Description') # convert our XML-like string of XMP data and strip of some of the fluff
+assert isinstance(xmp_dict, dict)
 
-thermal = Thermal(
+camera_altitude = float(xmp_dict.get('@drone-dji:RelativeAltitude').replace('+', '')) # since we need to know the size of the object in the image and the object in the image is the ground we'll need the distance to the ground to calculate it's size
+camera_heading = xmp_dict.get('@drone-dji:GimbalYawDegree') # we need this to orient the image relative to north
+
+GSDh = calculate_gsd(camera_altitude, CAMERA_SENSOR_HEIGHT, CAMERA_FOCAL_LENGTH, image_height) # how tall is a pixel mm
+GSDw = calculate_gsd(camera_altitude, CAMERA_SENSOR_WIDTH, CAMERA_FOCAL_LENGTH, image_width) # how wide is a pixel mm
+
+thermal = Thermal( # instance a Thermal object and pass it the SDK
     dirp_filename='plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libdirp.dll',
     dirp_sub_filename='plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libv_dirp.dll',
     iirp_filename='plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libv_iirp.dll',
     exif_filename='plugins/exiftool-12.35.exe',
-    dtype=np.float32,
+    dtype=numpy.float32,
 )
-temperature = thermal.parse_dirp2(image_filename=image_path, image_height=image_h, image_width=image_w, m2ea_mode=True) # m2ea refers to the mavic 2 enterprice advance drone
-assert isinstance(temperature, np.ndarray)
+temperature = thermal.parse_dirp2(image_filename=image_path, image_height=image_height, image_width=image_width, m2ea_mode=True) # m2ea refers to the mavic 2 enterprice advance drone
+assert isinstance(temperature, numpy.ndarray) # check we got the 2D array we're expecting
+mask_result_indexes = numpy.where(temperature > TEMPERATURE_MASK)
 
-mask_result_indexes = np.where(temperature > TEMPERATURE_MASK)
-
-print("  X,   Y | Tempurature")
-print("----------------------")
-for i in enumerate(mask_result_indexes[0]):
-    x = mask_result_indexes[1][i[0]]
-    y = i[1]
-    print(str(x) + ", " + str(y) + " | " + str(temperature[y][x]))
-
-GSDh = ((flight_h*1000) * camera_sensor_h) / (camera_focal_length * image_h) #make a function for this
-GSDw = ((flight_h*1000) * camera_sensor_w) / (camera_focal_length * image_w)
+print(GSDh*image_height/1000)
+print(GSDw*image_width/1000)
